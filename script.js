3870185510a4bef7dfc24544e63f17f005a8dc62
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-02-12 15:31:48 -0500

    Explicacion de mutaciones

--- script.js
+++ script.js
@@ -16,13 +16,49 @@ console.log(`Hola ${firstName} ${lastName}`)
 
 // String
 const tString = 'Texto'
+
 // Number
 const tNumber = 25
+
 // Boolean
 const tBoolean = true
+
 // Null
 const tNull = null
+
 // Undefined
 const tUndefined = undefined
+
+// ---------------------------------------------------------------
+
+// Mutaciones
+
+// Tipos de datos no mutables (Primitivos: String, Number, Boolean, Null, Undefined)
+// Los tipos de datos primitivos son inmutables
+firstName.toUpperCase()
+// Se obtiene el valor original porque es inmutable
+console.log(firstName)
+
+// Se crea una nueva variable con el valor modificado
+const firstNameUpper = firstName.toUpperCase()
+console.log(firstNameUpper)
+
+// Tipos de datos mutables (Objetos)
+const list = []
+list.push(1, 2, 3)
+console.log(list)
+
+const person = {
+    firstName: 'Victor',
+    lastName: 'Estupinan',
+    links: ['https://www.google.com', 'https://www.facebook.com']
+}
+
+person.firstName = 'Juan'
+
+const field = 'firstName'
+console.log(person[field])
+console.log(person.lastName)
+console.log(person.links[0])
 
 // ---------------------------------------------------------------
