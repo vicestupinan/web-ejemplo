aee97a99a5c0d008403abb9b3cad363398ba99cb
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-02-12 15:00:44 -0500

    Practica tipo de datos

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// Permite cambiar el valor de las variables
+// Variables
+
+// let permite cambiar el valor de las variables
 let firstName = 'Victor'
 firstName = 'Juan'
 
-// No permite cambiar el valor de las variables
+// const no permite cambiar el valor de las variables
 const lastName = 'Estupinan'
 // lastName = 'Perez' genera error porque const solo es de lectura
 
 console.log(`Hola ${firstName} ${lastName}`)
+
+// ---------------------------------------------------------------
+
+// Tipos de datos
+
+// String
+const tString = 'Texto'
+// Number
+const tNumber = 25
+// Boolean
+const tBoolean = true
+// Null
+const tNull = null
+// Undefined
+const tUndefined = undefined
+
+// ---------------------------------------------------------------
